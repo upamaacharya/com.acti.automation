5d8b6af0db5b4890680c96d5a6083e894b56ef2a
Acharya, Upama <upama.acharya@gmail.com>
2022-02-17 18:13:57 -0500

    Added Description about this file

--- src/test/java/com/actitime/testcase/BaseTest.java
+++ src/test/java/com/actitime/testcase/BaseTest.java
@@ -6,6 +6,14 @@ import org.testng.annotations.BeforeMethod;
 import com.actitime.base.DriverScript;
 import com.actitime.pages.LoginPage;
 
+/*
+ * Name: BaseTest
+ * Description: This is a parent script which has the set of prerequisite steps before executing actual test case
+ * Author: Upama
+ * Date Modified: 02/17/2022
+ */
+
+
 public class BaseTest extends DriverScript {
 	
 
